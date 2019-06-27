122cd6e4eda0426d55f36df1f9e3e629ebdd4581
Michael <welsh.11311@gmail.com>
2019-06-27 18:52:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      string: '',
+      para: '',
+      
+    }
+  }
+  checker(text) {
+    let count = 0;
+    for (let index in text) {
+      const char = text[index];
+      if (count >= 0) {
+        if (char === '(') {
+          count++;
+        }
+        if (char === ')') {
+          count--;
+        }
+        
+        if (count < 0) {
+          return (<mark>{text[index]}</mark>)
+        }
+      }
+    }
+      
+    if (count === 0) {
+      return true;
+    }
+
+  };
+  
+  update(event) {
+    this.setState({string: event.target.value});
+    this.checker(this.state.string);
+  }
+  
+  render() {
+    return (
+      <div>
+        <input
+          placeholder='text here' 
+          type='text'
+          value={this.state.string}
+          onChange={(event)=>this.update(event)}
+        />
+        <p>{this.state.string}</p>
+      </div>
+    );
+  };
 }
 
 export default App;
